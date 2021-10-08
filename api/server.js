655066d3d4aeecf9c5e25c5cef64c5ba502c6c80
stevenjhomem <sjhomem@outlook.com>
2021-10-08 07:14:44 -0500

    finsihed with the package json file and have the first connections between indexjs and erver.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("*", (req, res) => {
+    res.json({ api: "is up and running!" });
+  });
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+require("dotenv").config();
+
+const server = require("./api/server");
+
+const port = process.env.PORT ;
+
+server.listen(port, () => {
+  console.log(`\n API running on Port ${port}\n`);
+});
